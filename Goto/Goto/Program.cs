2e92b4eca21d47ac13430982129043319f8d6372
@jonatas.viana <56093381+jonatasvnascimento@users.noreply.github.com>
2021-07-27 18:31:04 -0300

    goto

---
+++ Goto/Goto/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace Goto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Duplicar linha  Ctrl + e,V
+            //Formatar Documento  Ctrl + K,D
+
+            int tempo = 0;
+            char escolha = '0';
+            string resp = "";
+
+            inicio:
+
+            Console.WriteLine("Viagens Rapidas LTDA");
+            Console.WriteLine("Belo Horizonte >>>> a >>>> São Paulo");
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("Escolha de Tansport:" +
+                "\n Avião:  [a]" +
+                "\n Carro:  [c]" +
+                "\n Onibus: [o]");
+
+            try{
+            escolha = char.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                goto inicio;
+            }
+
+
+            switch (escolha)
+            {
+                case 'a':
+                case 'A':
+                    tempo = 50;
+                    break;
+                case 'c':
+                case 'C':
+                    tempo = 420;
+                    break;
+                case 'o':
+                case 'O':
+                    tempo = 660;
+                    break;
+
+                default:
+                    tempo = -1;
+                    break;
+            }
+
+            if(tempo < 0)
+            {
+                Console.WriteLine("Veiculo não disponivel");
+                
+            }else
+            {
+                Console.WriteLine($"O tempo para e essa viagem e de {(float)tempo / 60}h");
+            }
+
+            Console.WriteLine("Deseja continuas?");
+            resp = Console.ReadLine().ToString().ToLower();
+
+            if (resp == "s" || resp == "")
+            {
+                goto inicio;
+            }
+            else
+            {
+                Console.WriteLine("Obrigado pela preferencia.");
+            }
+
+
+
+
+        }
+    }
+}
