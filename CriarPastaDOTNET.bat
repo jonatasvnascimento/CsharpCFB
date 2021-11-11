9041fe8b50c18517fb3be71f332e51537ced4460
@jonatas.viana <56093381+jonatasvnascimento@users.noreply.github.com>
2021-11-11 13:13:44 -0300

    Aula11

---
+++ CriarPastaDOTNET.bat
@@ -0,0 +1,1 @@
+cd C:\Users\jnascimento3\Documents\GitHub\CsharpCFB\Aulas && mkdir aula11 && cd aula11 && dotnet new console && cd C:\Users\jnascimento3\Documents\GitHub\CsharpCFB\Aulas\aula11 && csc Program.cs && cls && C:\Users\jnascimento3\Documents\GitHub\CsharpCFB\Aulas\aula11\Program.exe
---
+++ ExecurarSriptsPasta.bat
@@ -0,0 +1,1 @@
+cd C:\Users\jnascimento3\Documents\GitHub\CsharpCFB\Aulas\aula10 && csc Program.cs && cls && C:\Users\jnascimento3\Documents\GitHub\CsharpCFB\Aulas\aula10\Program.exe
