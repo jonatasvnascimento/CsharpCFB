d249cd625d9fa56243814fe781eb24414ebf08fb
@jonatas.viana <56093381+jonatasvnascimento@users.noreply.github.com>
2021-11-13 09:49:17 -0300

    aula15

--- Aulas/aula15/Program.cs
+++ Aulas/aula15/Program.cs
@@ -6,7 +6,45 @@ namespace aula15
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int tempo = 0;
+            char opcao = ' ';
+
+            Console.WriteLine("São Paulo / Rio de Janeiro");
+            Console.WriteLine("Escolha o tranporte: [a]Avião [c]Carro [o]Onibus");
+
+            opcao = char.Parse(Console.ReadLine());
+
+            if (opcao != 'a' && opcao != 'c' && opcao != 'o')
+            {
+                Console.WriteLine("Opção de transpote invalida");
+            }
+
+            switch (opcao)
+            {
+                case 'a':
+                case 'A':
+                    tempo = 30;
+                    break;
+                case 'c':
+                    tempo = 480;
+                    break;
+                case 'o':
+                    tempo = 660;
+                    break;
+                default:
+                    tempo = -1;
+                    break;
+            }
+
+            if (tempo < 0)
+            {
+                Console.WriteLine("Tranporte não informado");
+            }
+            else
+            {
+                Console.WriteLine("O tempo informado para a viagem e de {0}", tempo);
+            }
+
         }
     }
 }
