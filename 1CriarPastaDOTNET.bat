86bbd74b7a9a2d30c4a3c495b9ed8d9b73e247b1
@jonatas.viana <56093381+jonatasvnascimento@users.noreply.github.com>
2021-11-18 13:11:33 -0300

    aula22

--- 1CriarPastaDOTNET.bat
+++ 1CriarPastaDOTNET.bat
@@ -1,1 +1,2 @@
-cd C:\Users\jnascimento3\Documents\GitHub\CsharpCFB\Aulas && mkdir aula21 && cd aula21 && dotnet new console && cd C:\Users\jnascimento3\Documents\GitHub\CsharpCFB\Aulas\aula21 && csc Program.cs && cls && C:\Users\jnascimento3\Documents\GitHub\CsharpCFB\Aulas\aula21\Program.exe
+SET a = aula22
+cd C:\Users\jnascimento3\Documents\GitHub\CsharpCFB\Aulas && mkdir aula22 && cd aula22 && dotnet new console && cd C:\Users\jnascimento3\Documents\GitHub\CsharpCFB\Aulas\aula22 && csc Program.cs && cls && C:\Users\jnascimento3\Documents\GitHub\CsharpCFB\Aulas\aula22\Program.exe
--- 2ExecurarSriptsPasta.bat
+++ 2ExecurarSriptsPasta.bat
@@ -1,1 +1,2 @@
-cd C:\Users\jnascimento3\Documents\GitHub\CsharpCFB\Aulas\aula21 && csc Program.cs && cls && C:\Users\jnascimento3\Documents\GitHub\CsharpCFB\Aulas\aula21\Program.exe
+Set a = aula22
+cd C:\Users\jnascimento3\Documents\GitHub\CsharpCFB\Aulas\aula22 && csc Program.cs && cls && C:\Users\jnascimento3\Documents\GitHub\CsharpCFB\Aulas\aula22\Program.exe
