7a5dcba496e38529190c7bd33041aca552cd3c71
@jonatas.viana <56093381+jonatasvnascimento@users.noreply.github.com>
2021-11-19 08:21:20 -0300

    teste new project

---
+++ Projetos/projectCBF/projectCBF/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace projectCBF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 99999;
+            Console.WriteLine($"{num}Hello World!");
+        }
+    }
+}
+
