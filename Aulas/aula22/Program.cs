c84542ea32e67a8865c6aa7234b7a38ba540d30d
@jonatas.viana <56093381+jonatasvnascimento@users.noreply.github.com>
2021-11-19 07:18:24 -0300

    aula22

--- Aulas/aula22/Program.cs
+++ Aulas/aula22/Program.cs
@@ -6,7 +6,15 @@ namespace aula22
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] num = new int[5];
+            Random rand = new Random();
+            for (int i = 0; i < num.Length; i++)
+            {
+                num[i] = rand.Next(1, 10);
+                Console.Write("|{0}|", num[i]);
+            }
+
+
         }
     }
 }
