73a9b5d5fee9d12a8bbf08896efa228aae219e10
@jonatas.viana <56093381+jonatasvnascimento@users.noreply.github.com>
2021-12-02 10:14:52 -0300

    aula29

---
+++ Aulas/aula29/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace aula29
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
