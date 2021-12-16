fd8381a9610e7649e5b35c0b5ec7712c635c8cce
jonatasvnascimento <jonatasv.nascimento@gmail.com>
2021-12-16 15:33:13 -0300

    Aula31

---
+++ Aulas/aula31/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace aula31
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
