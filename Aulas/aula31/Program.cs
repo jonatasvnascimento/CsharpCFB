2e6eb63a63fac8ea20577d2ec130f52b00fe8eea
jonatasvnascimento <jonatasv.nascimento@gmail.com>
2021-12-17 08:56:07 -0300

    aula31

--- Aulas/aula31/Program.cs
+++ Aulas/aula31/Program.cs
@@ -4,9 +4,60 @@ namespace aula31
 {
     internal class Program
     {
+        
+        
+        public static class Player
+        {
+            public static string nome;
+            public static int energia;
+            public static bool vivo;
+            public static void iniciar(string n)
+            {
+                nome = n;
+                energia = 100;
+                vivo = true;
+            }
+            public static void info()
+            {
+                Console.WriteLine($"Nome...: {nome}");
+                Console.WriteLine($"energia: {energia}");
+                Console.WriteLine($"Vivo...: {vivo}");
+
+            }
+        }
+        class Inimigo
+        {
+            public static bool alerta;
+            public string nome;
+
+            public Inimigo(string n)
+            {
+                alerta = false;
+                nome = n;
+            }
+            public void info()
+            {
+                Console.WriteLine($"{alerta}");
+                Console.WriteLine($"{nome}");
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Player.iniciar("PLayer");
+            Player.info();
+
+            Inimigo i1 = new Inimigo("Inimigo1");
+            Inimigo i2 = new Inimigo("Inimigo2");
+            Inimigo i3 = new Inimigo("Inimigo3");
+
+            Inimigo.alerta = true;
+
+            i1.info();
+            i2.info();
+            i3.info();
+
+
+
         }
     }
 }
