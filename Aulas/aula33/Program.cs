35701ae74a98636777ae348ccc07c8bd08a5b3d3
jonatasvnascimento <jonatasv.nascimento@gmail.com>
2021-12-27 12:51:47 -0300

    aula33

---
+++ Aulas/aula33/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace aula33
+{
+    internal class Program
+    {
+        class Player
+        {
+            private int energia;
+            private string nome;
+
+            public Player(string nomePlayer)
+            {
+                this.nome = nomePlayer;
+            }
+            public int getEnergia()
+            {
+                return energia;
+            }
+            public string getNome()
+            {
+                return nome;
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Player p1 = new Player("Player");
+
+            Console.WriteLine($"Energia: {p1.getEnergia()}");
+            Console.WriteLine($"Nome...: {p1.getNome()}");
+
+
+        }
+    }
+}
